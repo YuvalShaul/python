f188075ecf3fd9c7e100190cd0636fa6f0b0d3c8
Yuval SHaul <yuval.shaul@gmail.com>
2025-02-22 17:15:15 +0200

    deleting files

--- language/6-modules/mod1.py
+++
@@ -1,8 +0,0 @@
-print('module mod1 imported')
-
-mod1_var = 15
-
-def mod1_func():
-    print("This is mod1_func from module mod1. War is: ", mod1_var)
-    
-
--- language/6-modules/mod2.py
+++
@@ -1,4 +0,0 @@
-print('I am module mod2')
-print('my module name is: ', __name__)
-    
-
